9d9ea1392cd87cb5b0ec581e129b1136fe1f456a
unknown <areczcki>
2019-08-19 23:54:29 -0300

    Tela de login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,40 +1,106 @@
-@extends('layout.layout')
-@section('title') Blog - Postagens @stop
-<form class="form-horizontal new-lg-form" id="loginform" action="/auth/login" method="post">
-	{{ csrf_field() }}
+<!DOCTYPE html>
+<html lang="en">
 
-	<div class="form-group  m-t-20">
-		<div class="col-xs-12">
-			<label>Endereço de Email</label> <input class="form-control"
-				type="text" required="" placeholder="Email" id="email" name="email">
-		</div>
-	</div>
+<head>
 
-	<div class="form-group">
-		<div class="col-xs-12">
-			<label>Senha</label> <input class="form-control" type="password"
-				required="" placeholder="Senha" id="senha" name="password">
-		</div>
-	</div>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	<div class="form-group">
-		<div class="col-md-12">
-			<div class="checkbox checkbox-info pull-left p-t-0">
-				<input id="checkbox-signup" type="checkbox"> <label
-					for="checkbox-signup"> Lembrar-me </label>
-			</div>
-			<a href="javascript:void(0)" id="to-recover"
-				class="text-dark pull-right"><i class="fa fa-lock m-r-5"></i>
-				Esqueceu a senha ?</a>
-		</div>
-	</div>
+    <title>SB Admin 2 - Login</title>
 
-	<div class="form-group text-center m-t-20">
-		<div class="col-xs-12">
-			<button
-				class="btn btn-info btn-lg btn-block btn-rounded text-uppercase waves-effect waves-light"
-				type="submit">Entrar</button>
-		</div>
-	</div>
+    <!-- Custom fonts for this template-->
+	<link href="{{ asset("assets/sb_admin/vendor/fontawesome-free/css/all.min.css")}}" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+          rel="stylesheet">
 
-</form>
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+	<link href="{{ asset("assets/sb_admin/css/sb-admin-2.min.css")}}" rel="stylesheet">
+
+</head>
+
+<body class="bg-gradient-primary">
+
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                </div>
+
+                                <form class="form-horizontal new-lg-form" id="loginform" action="/auth/login" method="post">
+                                    {{ csrf_field() }}
+                                    <div class="form-group">
+                                        <input type="email" id="email" name="email" class="form-control form-control-user"
+                                               id="exampleInputEmail" aria-describedby="emailHelp"
+                                               placeholder="Enter Email Address...">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" id="senha" name="password" class="form-control form-control-user"
+                                               id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="custom-control custom-checkbox small">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
+                                            <label class="custom-control-label" for="customCheck">Remember Me</label>
+                                        </div>
+                                    </div>
+
+                                    <button
+                                            class="btn btn-primary btn-user btn-block"
+                                            type="submit">Login
+                                    </button>
+                                    <hr>
+                                    <a href="index.html" class="btn btn-google btn-user btn-block">
+                                        <i class="fab fa-google fa-fw"></i> Login with Google
+                                    </a>
+                                    <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                        <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                    </a>
+                                </form>
+                                <hr>
+                                <div class="text-center">
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="register.html">Create an Account!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript-->
+<script src="{{ asset("assets/sb_admin/vendor/jquery/jquery.min.js") }}"></script>
+<script src="{{ asset("assets/sb_admin/vendor/bootstrap/js/bootstrap.bundle.min.js") }}"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="{{ asset("assets/sb_admin/vendor/jquery-easing/jquery.easing.min.js") }}"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="{{ asset("assets/sb_admin/js/sb-admin-2.min.js") }}"></script>
+
+</body>
+
+</html>
